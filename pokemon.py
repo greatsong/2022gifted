ad8fccbb2b0b05e1c255a83b44183360b2dbec8c
송석리(Song Sukree) <greatsong21@gmail.com>
2022-06-18 11:56:31 +0900

    색깔을 변경하였음

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,6 @@
 Web VPython 3.2
 images = ['https://cdn.econovill.com/news/photo/201603/285365_95988_038.png','https://cdn.banggooso.com/assets/images/game96/result/ESTJ.png']
-a = arrow(pos = vec(0,2,0), axis = vec(0,-1,0), color = color.green)
+a = arrow(pos = vec(0,2,0), axis = vec(0,-1,0), color = color.magenta)
 p = box(texture = images[0])
 g = box(pos = vec(1,0,0), texture = images[1])
 i = 0
@@ -11,10 +11,10 @@ while True :
     i = i + 1
     if i % 2 == 0 :
         p.color = color.white
-        g.color = color.green
+        g.color = color.magenta
     else :
-        p.color = color.green
+        p.color = color.magenta
         g.color = color.white
     if ' ' in k :
-        label(text = '당첨되셨습니다!', pos = vec(0,1.5,0))
+        label(text = '축하합니다! 당첨되셨습니다!', pos = vec(0,1.5,0))
         break
