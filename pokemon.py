634532ff5c118048528ea692ac57f6c6c1f4c4fc
송석리(Song Sukree) <greatsong21@gmail.com>
2022-06-18 11:50:46 +0900

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,20 @@
+Web VPython 3.2
+images = ['https://cdn.econovill.com/news/photo/201603/285365_95988_038.png','https://cdn.banggooso.com/assets/images/game96/result/ESTJ.png']
+a = arrow(pos = vec(0,2,0), axis = vec(0,-1,0), color = color.green)
+p = box(texture = images[0])
+g = box(pos = vec(1,0,0), texture = images[1])
+i = 0
+while True :
+    rate(10)
+    k = keysdown()
+    a.pos.x = i % 2
+    i = i + 1
+    if i % 2 == 0 :
+        p.color = color.white
+        g.color = color.green
+    else :
+        p.color = color.green
+        g.color = color.white
+    if ' ' in k :
+        label(text = '당첨되셨습니다!', pos = vec(0,1.5,0))
+        break
